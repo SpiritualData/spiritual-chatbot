a6c00a0e12b8a9ea6f876ae0bebbcf5f8d0bf94b
Kaushal Powar <90775147+kaushalpowar@users.noreply.github.com>
2023-06-23 19:47:18 +0530

    Update prepare_embeddings.py
    Added RecursiveTextSplitter instead of TextSplitter

--- spiritualchat/prepare_embeddings.py
+++ spiritualchat/prepare_embeddings.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from spiritualchat.vectorstores import pinecone
 
 
@@ -11,7 +11,7 @@ def prepare_embeddings():
 		1. Use the Notion API to get 'Description' field from each row in Spiritual Research (https://www.notion.so/spiritualdata/15b002ffae8e4ccdad4b55a8f619eea8?v=b32c8d520e144c568f1ac60af1cadac4) and Spiritual Experiences (https://www.notion.so/spiritualdata/6c8b84da23054462a3d8bdaa2e1c4968?v=9df4e3c4950744059ba1527d98e85bee).
 		2. Embed description using OpenAI ada 002 embedding with langchain.
 	"""
-	text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+	text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
 
 	texts = text_splitter.split_documents(documents)
 
